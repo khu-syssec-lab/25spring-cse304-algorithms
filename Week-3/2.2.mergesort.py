3c2c9cadf4f8f9e834454d729d93ffcb99b6c3e4
tim0700 <dlwogur200101@gmail.com>
2025-03-20 23:43:37 +0900

    분할정복_알고리즘
    1. Binary search (분할정복)
    2. mergesort
    3. mergesort2 (공간복잡도 개선)

--- Week-3/2.2.mergesort.py
+++ Week-3/2.2.mergesort.py
@@ -5,10 +5,43 @@ def merge(h: int, m: int, U: List[int], V: List[int], S: List[int]) -> None:
     assert sorted(V) == V
     
     i = j = k = 0
-    # Complete the code here
+    while i<h and j < m:
+        if U[i] < V[j]:
+            S[k] = U[i]
+            i+=1
+        else:
+            S[k]=V[j]
+            j+=1
+        k+=1
+
+    while i < h:
+        S[k] = U[i]
+        i += 1
+        k += 1
+
+    while j < m:
+        S[k] = V[j]
+        j += 1
+        k += 1
 
 def mergesort(n: int, S: List[int]) -> None:
     h = n // 2
     m = n - h
     if n > 1:
-        # Complete the code here
+        
+        U = [0]*h
+        V = [0]*m
+
+        for i in range(h):
+            U[i] = S[i]
+        
+        for i in range(m):
+            V[i] = S[h + i]  
+
+        mergesort(h,U)
+        mergesort(m,V)
+        merge(h,m,U,V,S)
+
+
+
+
--- Week-3/2.4.mergesort2.py
+++ Week-3/2.4.mergesort2.py
@@ -3,7 +3,30 @@ from typing import List
 def merge2(low: int, mid: int, high: int, S: List[int]) -> None:
     U = [0] * (high - low + 1)
     i, j, k = low, mid + 1, 0
-    # Complete the code here
+    #S에서 U로 비교하며 U에 정렬
+    while i <= mid and j <= high:
+        if S[i] < S[j]:
+            U[k] = S[i]
+            i += 1
+        else:
+            U[k] = S[j]
+            j += 1
+        k += 1    
+
+    # 정렬후 남은 요소들 처리
+    while i <= mid:
+        U[k] = S[i]
+        i += 1
+        k += 1
+        
+    while j <= high:
+        U[k] = S[j]
+        j += 1
+        k += 1
+    
+    # 완성된 임시 배열의 값을 원래 배열로 복사
+    for i in range(high - low + 1):
+        S[low + i] = U[i]
 
 def mergesort2(low: int, high: int, S: List[int]) -> None:
     if low < high:
